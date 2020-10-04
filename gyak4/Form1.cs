01aafb87768d19da30362cebc389db7cb25436eb
Dániel Csaba Szakács <daniel.szakacs@stud.uni-corvinus.hu>
2020-10-04 17:15:06 +0200

    7f8 commit

--- gyak4/Form1.cs
+++ gyak4/Form1.cs
@@ -98,6 +98,9 @@ namespace gyak4
                 values[counter, 8] = "";
                 counter++;
             }
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
         }
         private string GetCell(int x, int y)
         {
