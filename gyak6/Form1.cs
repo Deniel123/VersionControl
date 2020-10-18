1d2a04b6d62f259cc0828c3a2e284f2c9f02e0d0
Dániel Csaba Szakács <daniel.szakacs@stud.uni-corvinus.hu>
2020-10-18 23:23:56 +0200

    5f commit

--- gyak6/Form1.cs
+++ gyak6/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace gyak6
 {
@@ -19,11 +20,11 @@ namespace gyak6
         public Form1()
         {
             InitializeComponent();
-            GetExchangeRates();
+            RefreshData();
             dataGridView1.DataSource = Rates;
         }
 
-        public void GetExchangeRates()
+        public void RefreshData()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
@@ -35,6 +36,29 @@ namespace gyak6
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+        }
+
+        private void XMLfeldolg(string result)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
