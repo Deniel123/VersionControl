a52aec6a26da260d04cfd06180f923b1a513f853
Dániel Csaba Szakács <daniel.szakacs@stud.uni-corvinus.hu>
2020-11-15 19:43:06 +0100

    8f commit

---
+++ gyak8/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using gyak8.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak8.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
